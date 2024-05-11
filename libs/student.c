4655ae74cc62411d1eccaf7b42e81c60fee919ef
ArifDoloksaribu <143679155+ArifDoloksaribu@users.noreply.github.com>
2024-05-11 19:10:24 +0700

    026 - 051

--- libs/student.c
+++ libs/student.c
@@ -76,10 +76,9 @@ void move_student(struct student_t *_student, int size, struct dorm_t *_dorm, in
 }
 
 void dorm_empty(struct student_t *_student, int size, struct dorm_t *_dorm, int size_dorm){
- for (int a =0; a<size; a++){
-    _student[a].dorm = NULL;
- } 
- _dorm->residents_num = 0;
+    _student->dorm = NULL;
+    _dorm->residents_num--;
+
 }
 
 
